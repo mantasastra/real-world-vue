aac2a88037ff6386ab768803106f4d12f3fb0223
Mantas Astrauskas <mastrauskas4@gmail.com>
2021-01-29 11:15:54 +0000

    add navbar and sass

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,20 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link :to="{ name: 'event-list' }">List</router-link> |
-      <router-link :to="{ name: 'event-create' }">Create</router-link>
-    </div>
+    <nav-bar />
     <router-view />
   </div>
 </template>
+
+<script>
+import NavBar from '@/components/NavBar'
+
+export default {
+  name: 'App',
+  components: {
+    NavBar
+  }
+}
+</script>
 
 <style>
 html {
