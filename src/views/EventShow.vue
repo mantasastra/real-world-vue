4cc19da8d1de576d7129f690473764f8f46e9c10
Mantas Astrauskas <mastrauskas4@gmail.com>
2021-01-29 14:55:32 +0000

    Expand event show component

--- src/views/EventShow.vue
+++ src/views/EventShow.vue
@@ -1,12 +1,80 @@
 <template>
-  <h1>Showing event #{{ id }}</h1>
+  <div>
+    <div class="event-header">
+      <span class="eyebrow">@{{ event.time }} on {{ event.date }}</span>
+      <h1 class="title">{{ event.title }}</h1>
+      <h5>Organized by {{ event.organizer }}</h5>
+      <h5>Category: {{ event.category }}</h5>
+    </div>
+    <base-icon name="map">Location</base-icon>
+    <address>{{ event.location }}</address>
+    <h2>Event details</h2>
+    <p>{{ event.description }}</p>
+    <h2>
+      Attendees
+      <span class="badge -fill-gradient">{{
+        event.attendees ? event.attendees.length : 0
+      }}</span>
+    </h2>
+    <ul class="list-group">
+      <li
+        v-for="(attendee, index) in event.attendees"
+        :key="index"
+        class="list-item"
+      >
+        <b>{{ attendee.name }}</b>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
+import EventService from '@/services/EventService'
+
 export default {
   name: 'EventShow',
-  props: ['id']
+  props: ['id'],
+  data() {
+    return {
+      event: {}
+    }
+  },
+  created() {
+    EventService.getEvent(this.id)
+      .then(res => {
+        this.event = res.data
+      })
+      .catch(err => {
+        console.error(
+          `An error occurred when fetching event ${this.id}`,
+          err.response
+        )
+      })
+  }
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+.location {
+  margin-bottom: 0;
+}
+
+.location > .icon {
+  margin-left: 10px;
+}
+
+.event-header > .title {
+  margin: 0;
+}
+
+.list-group {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
+
+.list-group > .list-item {
+  padding: 1em 0;
+  border-bottom: solid 1px #e5e5e5;
+}
+</style>
